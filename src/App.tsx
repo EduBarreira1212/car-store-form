8137bc56a7bfe14e5d4a0ace39f40bd8cb08169e
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-29 18:48:56 -0300

    feat: styled component created to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import FormPage from './FormPage';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center
+`;
 
 function App() {
   return (
-    <div>
+    <Div>
       <FormPage/>
-    </div>
+    </Div>
   );
 }
 
