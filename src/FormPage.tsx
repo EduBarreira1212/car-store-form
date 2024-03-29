1a1e252c331596293465fcf5dab4b98bafb17bda
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-29 18:38:50 -0300

    feat: form created

--- src/FormPage.tsx
+++ src/FormPage.tsx
@@ -1,7 +1,24 @@
 const FormPage = () => {
     return (
         <div>
-            
+            <h1>Car store form</h1>
+            <form>
+                <label htmlFor="car-name">Car name:</label>
+                <input type="text" name="name" id="car-name"></input>
+                <label>Car Brand:</label>
+                <input type="text" name="brand" id="car-brand"/>
+                <label>Car category:</label>
+                <select name="category" id="car-category">
+                    <option value="Sedan">Sedan</option>
+                    <option value="Hatch">Hatch</option>
+                    <option value="SUV">SUV</option>
+                    <option value="Sport">Sport</option>
+                </select>
+                <label>Price:</label>
+                <input type="number" name="price" id="car-price"/>
+                <label>Car photo:</label>
+                <input type="file" name="photo" id="car-photo"/>
+            </form>
         </div>
     );
 }
