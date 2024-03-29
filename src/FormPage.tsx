2caa6e0b2c87dd664cda6573ed7806af6928bb6f
Eduardo Barreira <edubarreira1212@gmail.com>
2024-03-29 20:18:57 -0300

    feat: data and disabled state created

--- src/FormPage.tsx
+++ src/FormPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled, { css } from "styled-components";
 
 const Style = css`
@@ -16,17 +17,36 @@ const Form = styled.form`
     gap: 0.75vh;
 `;
 
+interface Idatastate {
+    name: string
+    brand: string
+    category: string
+    price: number
+    photo: any
+}
+
 const FormPage = () => {
+
+    const [data, setdata] = useState<Idatastate>({
+        name: "",
+        brand: "",
+        category: "",
+        price: 0,
+        photo: undefined
+    });
+    
+    const [disabled, setDisabled] = useState<boolean>(true);
+
     return (
         <Div>
             <h1>Car store form</h1>
             <Form>
                 <label htmlFor="car-name">Car name:</label>
-                <input type="text" name="name" id="car-name" placeholder="Ex: F-150" required/>
+                <input type="text" value={data.name} name="name" id="car-name" placeholder="Ex: F-150" required/>
                 <label>Car Brand:</label>
-                <input type="text" name="brand" id="car-brand" placeholder="Ex: Ford" required/>
+                <input type="text" value={data.brand} name="brand" id="car-brand" placeholder="Ex: Ford" required/>
                 <label>Car category:</label>
-                <select name="category" id="car-category" required>
+                <select value={data.category} name="category" id="car-category" required>
                     <option value="Pickup">Pickup</option>
                     <option value="Sedan">Sedan</option>
                     <option value="Hatch">Hatch</option>
@@ -34,10 +54,10 @@ const FormPage = () => {
                     <option value="Sport">Sport</option>
                 </select>
                 <label>Price:</label>
-                <input type="number" name="price" id="car-price" placeholder="Ex: 50000" required/>
+                <input type="number" value={data.price} name="price" id="car-price" placeholder="Ex: 50000" required/>
                 <label>Car photo:</label>
-                <input type="file" name="photo" id="car-photo" required/>
-                <input type="submit" value="Send"/>
+                <input type="file" value={data.photo} name="photo" id="car-photo" required/>
+                <input type="submit" value="Send" disabled={disabled}/>
             </Form>
         </Div>
     );
