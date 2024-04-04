1b1cf85da293b57216de0ed7081d0c82e98e9696
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-04 11:47:48 -0300

    feat: div stylized and button with on click created

--- src/ShowListPage.tsx
+++ src/ShowListPage.tsx
@@ -1,6 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Div = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
 const ShowListPage = () => {
+    const navigate = useNavigate();
     return (
-        <div></div>
+        <Div>
+            <button onClick={() => navigate("/form-page")}>Register a car</button>
+        </Div>
     );
 }
 
