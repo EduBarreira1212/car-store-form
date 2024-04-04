3d4e6600d7d22e46bf2e87cb23c7d022caf311bf
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-04 11:33:35 -0300

    feat: routers configured

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import FormPage from './FormPage';
+import ShowListPage from './ShowListPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ShowListPage/>,
+  },
+  {
+    path: "/form-page",
+    element: <FormPage/>
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
