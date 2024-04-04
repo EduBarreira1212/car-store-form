1e982593de7fa8e2f444dfbbed5fe35a4cf7e5c7
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-04 11:45:44 -0300

    cleanup: removing unnecessary import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import FormPage from './FormPage';
 import ShowListPage from './ShowListPage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
