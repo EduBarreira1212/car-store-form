402210a09abefbf6ee97470eac20b2d4889331c5
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-04 10:12:31 -0300

    feat: validation form function and error component added

--- src/FormPage.tsx
+++ src/FormPage.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import styled, { css } from "styled-components";
+import * as Yup from 'yup';
 
 const Style = css`
     display: flex;
@@ -33,6 +34,14 @@ interface Idatastate {
     photo: any
 }
 
+const ProductSchema = Yup.object().shape({
+    name: Yup.string().required().matches(/^[a-zA-Z0-9\s]+$/),
+    brand: Yup.string().required().matches(/^[a-zA-Z0-9\s]+$/),
+    category: Yup.string().required(),
+    price: Yup.number().required().positive(),
+    photo: Yup.string().required()
+})
+
 const FormPage = () => {
 
     const handleSubmit = async (formvalues: Idatastate) => {
@@ -53,13 +62,16 @@ const FormPage = () => {
             <h1>Car store form</h1>
             <Formik
                 initialValues={{name: "", brand: "", category: "", price: 0, photo: ""}}
+                validationSchema={ProductSchema}
                 onSubmit={(values) => {handleSubmit(values)}}
             >
                 <FormFormik>
                     <label htmlFor="car-name">Car name:</label>
                     <Field type="text" name="name" id="car-name" placeholder="Ex: F-150"/>
+                    <ErrorMessage name="name" component="div"/>
                     <label htmlFor="car-brand">Car Brand:</label>
                     <Field type="text" name="brand" id="car-brand" placeholder="Ex: Ford"/>
+                    <ErrorMessage name="brand" component="div"/>
                     <label htmlFor="car-category">Car category:</label>
                     <Field as="select" name="category" id="car-category">
                         <option value="Pickup">Pickup</option>
@@ -68,10 +80,13 @@ const FormPage = () => {
                         <option value="SUV">SUV</option>
                         <option value="Sport">Sport</option>
                     </Field>
+                    <ErrorMessage name="category" component="div"/>
                     <label htmlFor="car-price">Price:</label>
                     <Field type="number" name="price" id="car-price" placeholder="Ex: 50000"/>
+                    <ErrorMessage name="price" component="div"/>
                     <label htmlFor="car-photo">Car photo:</label>
                     <Field type="file" name="photo" id="car-photo"/>
+                    <ErrorMessage name="photo" component="div"/>
                     <InputSub type="submit" value="Send"/>
                 </FormFormik>
             </Formik>
