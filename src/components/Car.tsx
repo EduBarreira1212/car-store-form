f459818e0eccb318a7db05d71f1df791867a8f70
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-07 00:42:35 -0300

    feat: adding tags to render the car object

--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -8,7 +8,11 @@ const Car: React.FC<IpropsInterface> = ({children}) => {
 
     return (
         <div>
-            
+            <p>Name: {children.name}</p>
+            <p>Brand: {children.brand}</p>
+            <p>Category: {children.category}</p>
+            <p>price: {children.price}</p>
+            <p>Photo: {children.photo}</p>
         </div>
     );
 }
