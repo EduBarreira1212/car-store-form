3ca9ae0bc885ef8cf16511482f561807910c5468
Eduardo Barreira <edubarreira1212@gmail.com>
2024-04-07 00:48:17 -0300

    fix: minimum height defined

--- src/ShowListPage.tsx
+++ src/ShowListPage.tsx
@@ -6,7 +6,7 @@ import { Idatastate } from "./FormPage";
 import Car from "./components/Car";
 
 const Div = styled.div`
-    height: 100vh;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
